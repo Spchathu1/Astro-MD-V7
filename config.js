35074820961cd2bf7b47488d922a66a061ea89d9
Spchathu1 <127127514+Spchathu1@users.noreply.github.com>
2023-03-07 17:25:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
+ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'true' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
-INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
+FOOTER: process.env.FOOTER === undefined ? '𝐒𝐇𝐀𝐃𝐎𝐖 𝐗 𝐌𝐈𝐇𝐈𝐑𝐔𝐊' : process.env.FOOTER,
+CAPTION: process.env.CAPTION === undefined ? '*𝗦𝗛𝗔𝗗𝗢𝗪 𝗦𝗔𝗠𝗨𝗥𝗔𝗜*' : process.env.CAPTION,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
+INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*🧜‍♀️💬ALL INBOX MESSAGES  ARE BLOCKED BY SHADOW*' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
 SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
 };
